6c43ae7d262617029e8a56d283cde607cc444b24
Kwesi Dadson <samson.dadson@petratrust.com>
2019-12-05 12:05:52 +0000

    - removed unneeded code

--- python_pip_examples/websocket/exchange_websocket.py
+++
@@ -1,9 +0,0 @@
-from bitcoinaverage import ExchangeWebsocketClient
-
-if __name__ == '__main__':
-    secret_key = '' or input('Enter your secret key: ')
-    public_key = '' or input('Enter your public key: ')
-
-    print('Connecting to the exchange websocket...')
-    ws = ExchangeWebsocketClient(public_key, secret_key)
-    ws.exchange_data('bitstamp')
--- python_pip_examples/websocket/ticker_websocket.py
+++
@@ -1,9 +0,0 @@
-from bitcoinaverage import TickerWebsocketClient
-
-if __name__ == '__main__':
-    secret_key = '' or input('Enter your secret key: ')
-    public_key = '' or input('Enter your public key: ')
-
-    print('Connecting to the ticker websocket...')
-    ws = TickerWebsocketClient(public_key, secret_key)
-    ws.ticker_data('local', 'BTCUSD')
